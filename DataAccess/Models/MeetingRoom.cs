311f7d95d9ac0aa4fa71efc65160a564eac234d1
DmitriiPodlev <42978023+DmitriiPodlev@users.noreply.github.com>
2021-07-08 13:21:12 +0300

    Models

---
+++ DataAccess/Models/MeetingRoom.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Models
+{
+    public class MeetingRoom
+    {
+        public int Id { get; set; }
+        public List<DateTime> Dates { get; set; }
+        public string Location { get; set; }
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+    }
+}
