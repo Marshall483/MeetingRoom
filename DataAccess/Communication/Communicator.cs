9a22698a9e08467b81538fca5412a204b309dcfa
DmitriiPodlev <42978023+DmitriiPodlev@users.noreply.github.com>
2021-07-08 18:06:08 +0300

    Create communicator

---
+++ DataAccess/Communication/Communicator.cs
@@ -0,0 +1,126 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Communication
+{
+    // implementation of methods for accessing the database
+    public class Communicator : ICommunicator
+    {
+        // database context
+        private readonly ApplicationContext db;
+
+        public Communicator(ApplicationContext db)
+        {
+            this.db = db;
+        }
+
+        //add new room
+        public void AddRoom(MeetingRoom room)
+        {
+            db.MeetingRoom.Add(room);
+            db.SaveChanges();
+        }
+
+        // update room
+        public void EditRoom(MeetingRoom room)
+        {
+            db.MeetingRoom.Update(room);
+            db.SaveChanges();
+        }
+
+        // delete room by it's id
+        public void DeleteRoom(int id)
+        {
+            var room = db.MeetingRoom.Where(x => x.Id == id)
+                .FirstOrDefault();
+            db.MeetingRoom.Remove(room);
+            db.SaveChanges();
+        }
+
+        // get room by id
+        public MeetingRoom GetRoom(int id)
+        {
+            return db.MeetingRoom.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // get list of rooms
+        public List<MeetingRoom> GetRooms()
+        {
+            return db.MeetingRoom.ToList();
+        }
+
+        // add new booking
+        public void AddBooking(Booking booking)
+        {
+            db.Booking.Add(booking);
+            db.SaveChanges();
+        }
+
+        // update booking
+        public void EditBooking(Booking booking)
+        {
+            db.Booking.Update(booking);
+            db.SaveChanges();
+        }
+
+        // delete booking by id
+        public void DeleteBooking(int id)
+        {
+            var booking = db.Booking.Where(x => x.Id == id)
+               .FirstOrDefault();
+            db.Booking.Remove(booking);
+            db.SaveChanges();
+        }
+
+        // get booking by id
+        public Booking GetBooking(int id)
+        {
+            return db.Booking.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // get list of bookings
+        public List<Booking> GetBookings()
+        {
+            return db.Booking.ToList();
+        }
+
+        // add new technic
+        public void AddTechnicalSupport(TechnicalSupport technic)
+        {
+            db.TechnicalSupport.Add(technic);
+            db.SaveChanges();
+        }
+
+        // update technic
+        public void EditTechnicalSupport(TechnicalSupport technic)
+        {
+            db.TechnicalSupport.Update(technic);
+            db.SaveChanges();
+        }
+
+        // delete technic by id
+        public void DeleteTechnicalSupport(int id)
+        {
+            var technic = db.TechnicalSupport.Where(x => x.Id == id)
+               .FirstOrDefault();
+            db.TechnicalSupport.Remove(technic);
+            db.SaveChanges();
+        }
+
+        // get technic by id
+        public TechnicalSupport GetTechnic(int id)
+        {
+            return db.TechnicalSupport.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // get list of technics
+        public List<TechnicalSupport> GetTechnics()
+        {
+            return db.TechnicalSupport.ToList();
+        }
+    }
+}
---
+++ DataAccess/Communication/ICommunicator.cs
@@ -0,0 +1,31 @@
+﻿using DataAccess.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Communication
+{
+    /* interface to communicate with database.
+       There are all methods to make different interactions with data.
+    */
+    public interface ICommunicator
+    {
+        void AddRoom(MeetingRoom room);
+        void EditRoom(MeetingRoom room);
+        void DeleteRoom(int id);
+        MeetingRoom GetRoom(int id);
+        List<MeetingRoom> GetRooms();
+        void AddBooking(Booking booking);
+        void EditBooking(Booking booking);
+        void DeleteBooking(int id);
+        Booking GetBooking(int id);
+        List<Booking> GetBookings();
+        void AddTechnicalSupport(TechnicalSupport technic);
+        void EditTechnicalSupport(TechnicalSupport technic);
+        void DeleteTechnicalSupport(int id);
+        TechnicalSupport GetTechnic(int id);
+        List<TechnicalSupport> GetTechnics();     
+    }
+}
